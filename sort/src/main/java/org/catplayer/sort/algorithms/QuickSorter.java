3f7cfa4eead7bc87a7a435a58bc32bfed8c8f3db
catplayer <ericma9512@gmail.com>
2020-06-06 03:30:29 +0800

    improve the compare code

--- sort/src/main/java/org/catplayer/sort/algorithms/QuickSorter.java
+++ sort/src/main/java/org/catplayer/sort/algorithms/QuickSorter.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 /**
  * quick sort
+ * <p>
+ * the point is
+ * <li>every time confirm an element's absolute position</li>
+ * <li>left part are all less than the element</li>
+ * <li>right part are all greater than the element</li>
+ * and continue sort the left and right part util all elements positions are confirmed
+ * <p>
  * o(N log N)
  *
  * @author catplayer
@@ -28,30 +35,86 @@ public class QuickSorter extends Sorter {
 
     }
 
-    private int absolutePosition(Comparable[] original, int low, int high) {
-        //the target for confirming its correct position
-        Comparable comparedTarget = original[low];
-        int lessIndex = low;
-        int greaterIndex = high + 1;
-        while (true) {
-            while (smaller(original[++lessIndex], comparedTarget)) {
-                if (lessIndex == high) {
-                    break;
-                }
-            }
-            while (smaller(comparedTarget, original[--greaterIndex])) {
-                if (greaterIndex == low) {
-                    break;
-                }
-            }
-            if (lessIndex >= greaterIndex) {
-                break;
-            }
-            exchange(original, lessIndex, greaterIndex);
-        }
-        exchange(original, greaterIndex, low);
+//    /**
+//     * the Algorithms fourth edition's version
+//     *
+//     * @param original original
+//     * @param low      low
+//     * @param high     high
+//     * @return the low index's element's absolute position
+//     */
+//    private int absolutePosition(Comparable[] original, int low, int high) {
+//        //the target for confirming its correct position
+//        Comparable comparedTarget = original[low];
+//        int lessIndex = low;
+//        int greaterIndex = high + 1;
+//        while (true) {
+//            while (smaller(original[++lessIndex], comparedTarget)) {
+//                if (lessIndex == high) {
+//                    break;
+//                }
+//            }
+//            while (smaller(comparedTarget, original[--greaterIndex])) {
+//                if (greaterIndex == low) {
+//                    break;
+//                }
+//            }
+//            if (lessIndex >= greaterIndex) {
+//                break;
+//            }
+//            exchange(original, lessIndex, greaterIndex);
+//        }
+//        exchange(original, greaterIndex, low);
+//
+//        return greaterIndex;
+//
+//    }
 
-        return greaterIndex;
+    /**
+     * catplayer's version
+     *
+     * @param original original
+     * @param low      low
+     * @param high     high
+     * @return the low index's element's absolute position
+     */
+    private int absolutePosition(Comparable[] original, int low, int high) {
+
+        Comparable target = original[low];
+        int less = low + 1;
+        int greater = high;
+        //do exchange
+        while (less < greater) {
+            //find the less part has a greater one
+            if (smaller(target, original[less])) {
+                //check the greater and find the less one
+                while (less < greater) {
+                    //find the less one, and move forward less, move backward grater
+                    if (smaller(original[greater], target)) {
+                        //exchange them
+                        exchange(original, less, greater);
+                        //move index
+                        less++;
+                        greater--;
+                        break;
+                    }
+                    //move backward
+                    greater--;
+                }
+            } else {
+                //move forward
+                less++;
+            }
+        }
+
+        //compare and swap
+        if (smaller(target, original[less])) {
+            exchange(original, low, less - 1);
+            return less - 1;
+        } else {
+            exchange(original, low, less);
+            return less;
+        }
 
     }
 
